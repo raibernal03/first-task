1039717ac40db8577f8720c1b9208c2905f47d96
Naarai Bernal <110580725+raibernal03@users.noreply.github.com>
2026-02-12 15:26:36 -0600

    init

---
+++ src/main/java/exercises/Main.java
@@ -0,0 +1,10 @@
+package exercises;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+        System.out.println("Hello, World!");
+    }
+}
